bda216480b058f759ca88cb143a9e2b67eb1f4b0
unknown <Chi@DESKTOP-BAI6EA6.alo360.local>
2017-08-21 12:29:54 +0700

    test

---
+++ README.md
@@ -0,0 +1,22 @@
+# init1808
+
+facebook.com/vanphoUEL
+
+vanpho01@gmail.com
+
+01694472176
+
+https://www.facebook.com/groups/khoaphamonline/
+
+https://www.facebook.com/groups/705050756358202/?source=create_flow
+
+// 
+
+MEAN -> NodeJS MongoDB ExpressJS Angular
+
+// Webpage webapp
+https://www.youtube.com/watch?v=7x1PDHsQyGw&list=PLzrVYRai0riRDreiNl_QJOc9BEq3GE7Yp
+
+// -> Express -> docs
+// NodeJS -> Javascript || Express
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript
---
+++ a.js
@@ -0,0 +1,25 @@
+console.log('hello world!');
+//khai bao bien
+//var khai bao cu, let khai bao moi
+//y=100;
+//var x=0;
+//let z=10;
+//hang là const
+//const a=100;
+    //a=10;
+    //const a="5";
+    //const b =5;
+//console.log(a-b);//=> 5
+//console.log(a==b);//=> true
+//console.log(a===b);//=> false
+//console.log(parseInt(a)+b);
+//console.log(+a+b);
+//console.log(a-0-b);
+//console.log(eval('45+55'));
+
+const a=1;
+let b;//console.log(b); //undefined
+const c=null;//console.log(c);
+
+//console.log(5!="5");
+console.log(typeof("5"));
